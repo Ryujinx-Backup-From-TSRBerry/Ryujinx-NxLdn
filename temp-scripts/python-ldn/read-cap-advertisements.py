535709365cb0358fa8bffee58108f3b6ad196f4a
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-05-30 16:51:09 +0200

    Add temp custom scripts to make development easier

---
+++ temp-scripts/python-ldn/read-cap-advertisements.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from scapy.all import *
+import ldn
+import json
+import base64
+
+cap_data = rdpcap("debug-cap.pcap")
+switch_pkts = cap_data.filter(lambda x: len(x) > 1000 and x.layers()[
+                              1] == scapy.layers.dot11.Dot11FCS and x.SC == 127)
+
+
+class LdnEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, ldn.AdvertisementFrame):
+            ad_dict = obj.__dict__
+            ad_dict["header"] = ad_dict["header"].__dict__
+            ad_dict["info"] = ad_dict["info"].__dict__
+            ad_dict["header"]["ssid"] = base64.b64encode(
+                ad_dict["header"]["ssid"]).decode()
+            ad_dict["nonce"] = base64.b64encode(ad_dict["nonce"]).decode()
+            ad_dict["info"]["key"] = base64.b64encode(
+                ad_dict["info"]["key"]).decode()
+            ad_dict["info"]["application_data"] = base64.b64encode(
+                ad_dict["info"]["application_data"]).decode()
+            participants = ad_dict["info"]["participants"].copy()
+            ad_dict["info"]["participants"] = []
+            for participant in participants:
+                participant_dict = participant.__dict__
+                participant_dict["mac_address"] = ":".join(
+                    [hex(x)[2:].zfill(2) for x in participant_dict["mac_address"].fields]).upper()
+                ad_dict["info"]["participants"].append(participant_dict)
+            return ad_dict
+        return super().default(obj)
+
+
+num = 0
+for pkt in switch_pkts:
+    print(f"Working on packet: {num}")
+    frame = ldn.AdvertisementFrame()
+    frame.decode(raw(pkt)[53:-4])
+    print("\n------\n")
+    print(json.dumps(frame, indent=2, cls=LdnEncoder))
+    print("<----\n")
+    num += 1
