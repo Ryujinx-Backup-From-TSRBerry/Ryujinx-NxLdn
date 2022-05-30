329f8a619c165337dc81fd844cfcd142a4296a97
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-05-31 00:07:44 +0200

    Add frame headers & packet types
    Move Nx helper structs into Types/

---
+++ src/Ryujinx.HLE/HOS/Services/Ldn/UserServiceCreator/NxLdn/Packets/HeaderFields.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.NxLdn.Packets
+{
+    // Common network header fields used for LDN
+    internal readonly struct HeaderFields
+    {
+        // Nintendo OUI
+        internal static readonly byte[] Oui = new byte[] { 0x00, 0x22, 0xAA };
+
+        // https://github.com/kinnay/LDN/blob/15ab244703eb949be9d7b24da95a26336308c8e9/ldn/__init__.py#L704
+        // https://github.com/kinnay/NintendoClients/wiki/LDN-Protocol#advertisement-frame
+        public static readonly ActionFrameHeader Action = new ActionFrameHeader() {
+            Category = 0x7F,
+            Oui = Oui,
+            ProtocolId = 0x04,
+            PacketType = PacketType.Advertisement
+        };
+
+        // https://github.com/kinnay/NintendoClients/wiki/LDN-Protocol#authentication-frame
+        public static readonly DataFrameHeader Authentication = new DataFrameHeader {
+            Oui = Oui,
+            PacketType = PacketType.Authentication
+        };
+    }
+}
