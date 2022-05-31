56d60dddbe38f180ea78033e5386d6b49a20ed54
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-05-31 19:06:24 +0200

    Add authentication challenge

---
+++ src/Ryujinx.HLE/HOS/Services/Ldn/UserServiceCreator/NxLdn/Types/AuthenticationPayload.cs
@@ -0,0 +1,5 @@
+namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.NxLdn.Types {
+    public interface AuthenticationPayload {
+
+    }
+}
---
+++ src/Ryujinx.HLE/HOS/Services/Ldn/UserServiceCreator/NxLdn/Types/AuthenticationRequest.cs
@@ -0,0 +1,14 @@
+using Ryujinx.Common.Memory;
+using System.Runtime.InteropServices;
+
+namespace Ryujinx.HLE.HOS.Services.Ldn.UserServiceCreator.NxLdn.Types {
+    [StructLayout(LayoutKind.Sequential, Size = 64)]
+    public struct AuthenticationRequest : AuthenticationPayload {
+        // I don't understand why this size is not 33
+        public Array32<byte> UserName;
+
+        public ushort AppVersion;
+
+        private Array30<byte> _pad1;
+    }
+}
