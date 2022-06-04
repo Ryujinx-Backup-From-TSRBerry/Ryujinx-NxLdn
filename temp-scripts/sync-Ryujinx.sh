7ff02fd1e4b30a1730c071881548645cba16d9b8
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-06-04 22:19:45 +0200

    Add sync script for main Ryujinx repo

---
+++ temp-scripts/sync-Ryujinx.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+script_dir=$(dirname $(realpath ${BASH_SOURCE[0]}))
+nxldn_dir=$(realpath $script_dir/../)
+
+echo $nxldn_dir
+
+git stash
+
+cd $1
+
+git pull
+dotnet build -c Release
+
+rsync -avP \
+    --exclude ".git" \
+    --exclude ".github" \
+    --exclude "README.md" \
+    --exclude "LICENSE.txt" \
+    --exclude "**/bin" \
+    --exclude "**/obj" \
+    ./ $nxldn_dir
+
+cd -
+
+git restore .
+git stash apply
