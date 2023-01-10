07ef561ca7baac09debda9dbd9f98d2de2a5841b
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2023-01-10 03:24:14 +0100

    Add setcap command to Ryujinx.sh

--- distribution/linux/Ryujinx.sh
+++ distribution/linux/Ryujinx.sh
@@ -11,4 +11,6 @@ if [ -f "$SCRIPT_DIR/Ryujinx.Headless.SDL2" ]; then
     RYUJINX_BIN="Ryujinx.Headless.SDL2"
 fi
 
+sudo setcap cap_net_admin,cap_net_raw+ep "$SCRIPT_DIR/$RYUJINX_BIN"
+
 env DOTNET_EnableAlternateStackCheck=1 "$SCRIPT_DIR/$RYUJINX_BIN" "$@"
